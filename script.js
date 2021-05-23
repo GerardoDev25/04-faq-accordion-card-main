4720595902ba883275f4ef884c12b36006def383
gerardodev25 <gerardodev25@gmail.com>
2021-05-22 20:42:47 -0400

    add logic

---
+++ script.js
@@ -0,0 +1,26 @@
+const divQuestions = document.getElementById("query");
+
+const selecQuery = (e) => {
+   const paragraph =
+      e.target.parentElement.nextSibling.nextElementSibling;
+
+   const icon =
+      e.target.parentElement.lastChild.previousElementSibling;
+
+   if (paragraph.classList.contains("no-active")) {
+      paragraph.classList.remove("no-active");
+      paragraph.classList.add("active");
+
+      icon.classList.remove("fa-chevron-up");
+      icon.classList.add("fa-chevron-down");
+   } else {
+      paragraph.classList.add("no-active");
+      paragraph.classList.remove("active");
+
+      icon.classList.add("fa-chevron-up");
+      icon.classList.remove("fa-chevron-down");
+   }
+   e.stopPropagation();
+};
+
+divQuestions.addEventListener("click", selecQuery);
